0237b0139555bbcffc73c7b68ea5c8567721674c
Abigail-Woolf <65195902+Abigail-Woolf@users.noreply.github.com>
2020-08-08 13:27:48 -0700

    adding last files

--- Earthquake_Challenge/static/js/logicStep2.js
+++ Earthquake_Challenge/static/js/logicStep2.js
@@ -1,6 +1,8 @@
 // Add consol.log to check to see if our code is working.
 console.log("working");
 
+
+
 // We create the tile layer that will be the background of our map.
 let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/streets-v11/tiles/{z}/{x}/{y}?access_token={accessToken}', {
 attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery (c) <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -21,14 +23,6 @@ let baseMaps = {
     "Satellite Streets": satelliteStreets
 };
 
-// Create the earthquake layer for our map.
-let earthquakes = new L.layerGroup();
-//We define an object that contains the overlays
-//This overlay will be visible all the time. 
-let overlays = {
-  Earthquakes: earthquakes
-};
-
 // Create the map object with a center and zoom level.
 let map = L.map('mapid', {
     center: [39.5, -98.5],
@@ -37,7 +31,7 @@ let map = L.map('mapid', {
 })
 
 // Pass our map layers into our layers control and add the layers control to the map
-L.control.layers(baseMaps, overlays).addTo(map);
+L.control.layers(baseMaps).addTo(map);
 
 
 //Grabbing our GeoJSON data.
@@ -46,33 +40,13 @@ function styleInfo(feature) {
         return {
             opacity: 1,
             fillOpacity: 1,
-            fillColor: getColor(feature.properties.mag),
+            fillColor: "#ffae42",
             color: "000000",
             radius: getRadius(feature.properties.mag),
             stroke: true,
             weight: 0.5
     };
 }
-// This function determines the color of the circle based on the magnitude of the earthquake.
-function getColor(magnitude) {
-    if (magnitude > 5) {
-      return "#ea2c2c";
-    }
-    if (magnitude > 4) {
-      return "#ea822c";
-    }
-    if (magnitude > 3) {
-      return "#ee9c00";
-    }
-    if (magnitude > 2) {
-      return "#eecc00";
-    }
-    if (magnitude > 1) {
-      return "#d4ee00";
-    }
-    return "#98ee00";
-  }
-
 function getRadius(magnitude) {
     if (magnitude === 0) {
         return 1;
@@ -81,20 +55,22 @@ function getRadius(magnitude) {
 }
 // Creating a GeoJSON layer with the retrieved data.
 L.geoJson(data, {
-  // We turn each feature into a circleMarker on the map.
-  pointToLayer: function(feature, latlng) {
-      console.log(data);
-      return L.circleMarker(latlng);
-    },
-  // We set the style for each circleMarker using our styleInfo function.
-style: styleInfo,
-  // We create a popup for each circleMarker to display the magnitude and
-  //  location of the earthquake after the marker has been created and styled.
-  onEachFeature: function(feature, layer) {
-  layer.bindPopup("Magnitude: " + feature.properties.mag + "<br>Location: " + feature.properties.place);
-}
-}).addTo(earthquakes);
 
-//Then we add the earthquake layer to our map
-earthquakes.addTo(map);
+    // We turn each feature into a circleMarker on the map.
+    
+    pointToLayer: function(feature, latlng) {
+                  console.log(data);
+                  return L.circleMarker(latlng);
+            },
+            style: styleInfo
+        }).addTo(map);
     });
+
+
+
+
+
+
+ 
+
+
